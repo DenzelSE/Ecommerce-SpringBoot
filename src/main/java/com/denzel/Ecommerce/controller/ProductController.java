748eae28e68508d2483b8a9da06cb561de1d2447
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-01-15 15:55:06 +0200

    all products endpoint path created

--- src/main/java/com/denzel/Ecommerce/controller/ProductController.java
+++ src/main/java/com/denzel/Ecommerce/controller/ProductController.java
@@ -1,14 +1,29 @@
 package com.denzel.Ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.denzel.Ecommerce.model.Product;
+import com.denzel.Ecommerce.service.ProductService;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
+    @Autowired  
+    public ProductService service;
     
     @RequestMapping("/")
     public String home(){
         return "Welcome to Ecommerce";
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
     }
 }
---
+++ src/main/java/com/denzel/Ecommerce/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.denzel.Ecommerce.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.denzel.Ecommerce.model.Product;
+import com.denzel.Ecommerce.repository.ProductRepository;
+
+
+@Service
+public class ProductService {
+
+    @Autowired
+    public ProductRepository repository;
+    
+    public List<Product> getAllProducts(){
+        return repository.findAll();
+    }
+}
